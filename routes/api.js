5bbe27da0b7ad90b83cebb8a989129dc004fd8b7
Hassan Ali <hassanali5062@gmail.com>
2019-05-17 11:34:22 +0500

    final with fb login

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const bodyParser = require('body-parser');
+const nodeFetch = require('node-fetch');
 const userController = require('../controllers/userController');
 
 // create application/json parser
@@ -9,10 +10,10 @@ let user = new userController();
 
 var router = express.Router();
 
-router.use((req,res,next)=>{
+router.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
@@ -43,7 +44,7 @@ router.put('/updateUser', jsonParser, async (req, res) => {
   if (!req.body)
     return res.status(400);
   await user.updateUser(req.body);
-   res.json('updated succesfully');
+  res.json('updated succesfully');
 });
 
 /* Delete a singal user */
@@ -51,6 +52,27 @@ router.delete('/deleteUser', async (req, res) => {
   user.deleteUser(req.query.id);
   res.json(`user is deleted successfully`);
   console.log('deleted succesfully');
+});
+
+router.post('/fb-login', jsonParser, async (req, res) => {
+  console.log('in post of fb lognin')
+  console.log('BODY=>>>' + req.body);
+  const {
+    accessToken,
+    userID,
+    userObj
+  } = req.body;
+  const fbres = await nodeFetch(`https://graph.facebook.com/v3.3/me?access_token=${accessToken}&method=get&pretty=0&sdk=joey&suppress_http_code=1`);
+  fbResJson = await fbres.json();
+  console.log(fbResJson);
+  if (fbResJson.id === userID) {
+    //a valid user
+    user.addNew(userObj);
+    res.json('valid user and done adding');
+  } else {
+    //some bad user
+    res.json('in valid user access token');
+  }
 });
 
 module.exports = router;
